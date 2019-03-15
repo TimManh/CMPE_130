7f71724680018374842eb643e1102125c21c90b4
Thinh Manh <timmanh93@gmail.com>
2019-03-15 12:45:17 -0700

    Comment added for hoare and lomuto sorting

--- QuickSortHoare.py
+++ QuickSortHoare.py
@@ -1,22 +1,30 @@
-# Ask professor
+# Main method of quick Sort
 def quickSort(a,l,h):
     if l<h:
         p=hopartition(a,l,h)
         quickSort(a,l,p)
         quickSort(a,p+1,h)
+
+# Hoare partition
 def hopartition(a,l,h):
     i=l
     j=h
     pivot=a[l]
+    # i and j are at the two ends of the array
     while True:
+        # Check the value at index i and j with pivot
+        # if true, increase i and decrease j
         while a[i]<pivot and i<h:
             i+=1
         while a[j]>pivot and j>l:
             j-=1
+        # The loop stop when i >=j
         if i>=j:
             break
+        # Swap the value between index i and j
         a[i],a[j]=a[j],a[i]
     return j
+    
 a=[12,2,31,75,1,5,36,99,71,305,29,301,3]
 quickSort(a,0,len(a)-1)
 print(a)
--- QuickSortLomuto.py
+++ QuickSortLomuto.py
@@ -1,17 +1,25 @@
-def luQuickSort(a,l,h):
+# Main method for quick Sort
+def loQuickSort(a,l,h):
     if l<h:
         p=partition(a,l,h)
-        luQuickSort(a,l,p-1)
-        luQuickSort(a,p+1,h)
+        loQuickSort(a,l,p-1)
+        loQuickSort(a,p+1,h)
+
+#Lomuto Partition
 def partition(a,l,h):
+    # Pivot always the last element of the array
     pi=a[h]
     i=l
+    # Iterate j and find value greater than pivot
+    # Swap the value and increment i
     for j in range(l,h):
         if a[j]<pi:
             a[i],a[j]=a[j],a[i]
             i+=1
+    # Swap the value of i index and last index
     a[i],a[h]=a[h],a[i]
     return i
+    
 a=[12,2,31,75,1,5,36,99,71,305,29,301]
-luQuickSort(a,0,len(a)-1)
+loQuickSort(a,0,len(a)-1)
 print(a)
